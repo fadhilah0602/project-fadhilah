46680ae8d5d87a79490aa1b5e5b05208cd1cb702
fadhilah0602 <fadhillahfebriani13@gmail.com>
2021-06-03 15:31:35 +0700

    mengganti logo

--- resources/views/layouts/parts/header.blade.php
+++ resources/views/layouts/parts/header.blade.php
@@ -1,7 +1,7 @@
 <header class="text-gray-600 body-font">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <a><img src="/aset/logotii.png"></a>
+        <a><img src="/aset/swim.png"></a>
         <span class="ml-3 text-xl">Sistem Informasi Pegawai</span>
       </a>
       <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-gray-400	flex flex-wrap items-center text-base justify-center">
