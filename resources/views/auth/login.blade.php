76fa3ded89dc9769ee74e045af063b3b565eb2c2
fadhilah0602 <fadhillahfebriani13@gmail.com>
2021-06-04 16:52:56 +0700

    Mengubah Kata pada Halaman Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,12 +5,11 @@
         <div class="container mx-auto flex px-5 md:flex-row flex-col items-center">
             <div
                 class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Welcome to Employee Information
-                    System
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Selamat Datang di Sistem Informasi Undangan Pegawai PT Sewiwi Indonesia
                     {{-- <br class="hidden lg:inline-block">readymade gluten --}}
                 </h1>
                 <p class="mb-8 leading-relaxed">
-                    Sistem Informasi Pegawai
+                {{-- Sistem Informasi Pegawai --}}
                 </p>
                 {{-- <div class="flex justify-center">
           <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
